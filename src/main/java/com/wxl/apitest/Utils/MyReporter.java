a6b984f47f64347dca048db08756c6ca44510be2
xlWang1 <1448039778@qq.com>
2025-03-22 23:58:15 +0800

    替换css

---
+++ src/main/java/com/wxl/apitest/Utils/MyReporter.java
@@ -0,0 +1,14 @@
+package com.wxl.apitest.Utils;
+import com.aventstack.extentreports.ExtentTest;
+public class MyReporter {
+    public static ExtentTest report;
+    private static String testName;
+
+    public static String getTestName() {
+        return testName;
+    }
+
+    public static void setTestName(String testName) {
+        MyReporter.testName = testName;
+    }
+}
